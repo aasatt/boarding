db401cd94bc526e887af21e78e95bc653a59fc2c
Aaron Satterfield <aasatt@gmail.com>
2016-02-26 17:38:20 -0800

    allow iframe

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Boarding
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+  config.action_dispatch.default_headers = {
+    'X-Frame-Options' => 'ALLOWALL'
+  }
   end
 end
